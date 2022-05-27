4cd4a93f07bb50877ae166c6748025bc21b1a3e8
DniweTamp <egorbarbolin4@gmail.com>
2022-05-27 14:02:23 +0300

    fixed first dorm floor + moved to aemulus

--- build.bat
+++ build.bat
@@ -1,64 +1,51 @@
-set PAKPACK="path\to\PAKPack.exe"
 set COMPILER="path\to\AtlusScriptCompiler.exe"
-set OUTPUT_PATH=path\to\modcompendiumoutput\Data
+set OUTPUT_PATH=path\to\aemulus\p3p\modmenu
 
-if not exist "%OUTPUT_PATH%" mkdir "%OUTPUT_PATH%"
-if not exist "%OUTPUT_PATH%\data\field2d\bg" mkdir "%OUTPUT_PATH%\data\field2d\bg"
+%COMPILER% .\hook\field_bf\field.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\init_free\field.bf" -Hook
 
-for %%f in (".\original\abin\*.abin") do copy "%%f" "%OUTPUT_PATH%\data\field2d\bg"
-
-for %%f in (.\hook\*.flow) do %COMPILER% "%%f" -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\%%~nf.bf" -Hook
-
-	goto:replacing
-:deletebfs
-for %%f in ("%OUTPUT_PATH%\data\field2d\bg\*.bf") do del "%%f"
-PAUSE
-	goto:eof
-
-
-:replacing
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_01.abin" h06_01.bf "%OUTPUT_PATH%\data\field2d\bg\h06_01.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_01.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_02.abin" h06_02.bf "%OUTPUT_PATH%\data\field2d\bg\h06_02.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_02.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_03.abin" h06_03.bf "%OUTPUT_PATH%\data\field2d\bg\h06_03.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_03.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_04.abin" h06_04.bf "%OUTPUT_PATH%\data\field2d\bg\h06_04.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_04.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_05.abin" h06_05.bf "%OUTPUT_PATH%\data\field2d\bg\h06_05.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_05.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_07.abin" h06_07.bf "%OUTPUT_PATH%\data\field2d\bg\h06_07.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_07.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_11.abin" h06_11.bf "%OUTPUT_PATH%\data\field2d\bg\h06_11.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_11.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_16.abin" h06_16.bf "%OUTPUT_PATH%\data\field2d\bg\h06_16.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_16.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b06_20.abin" h06_20.bf "%OUTPUT_PATH%\data\field2d\bg\h06_20.bf" "%OUTPUT_PATH%\data\field2d\bg\b06_20.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b07_02.abin" h07_02.bf "%OUTPUT_PATH%\data\field2d\bg\h07_02.bf" "%OUTPUT_PATH%\data\field2d\bg\b07_02.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b07_03.abin" h07_03.bf "%OUTPUT_PATH%\data\field2d\bg\h07_03.bf" "%OUTPUT_PATH%\data\field2d\bg\b07_03.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b07_04.abin" h07_04.bf "%OUTPUT_PATH%\data\field2d\bg\h07_04.bf" "%OUTPUT_PATH%\data\field2d\bg\b07_04.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b07_05.abin" h07_05.bf "%OUTPUT_PATH%\data\field2d\bg\h07_05.bf" "%OUTPUT_PATH%\data\field2d\bg\b07_05.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b07_06.abin" h07_06.bf "%OUTPUT_PATH%\data\field2d\bg\h07_06.bf" "%OUTPUT_PATH%\data\field2d\bg\b07_06.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b07_09.abin" h07_09.bf "%OUTPUT_PATH%\data\field2d\bg\h07_09.bf" "%OUTPUT_PATH%\data\field2d\bg\b07_09.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b07_19.abin" h07_19.bf "%OUTPUT_PATH%\data\field2d\bg\h07_19.bf" "%OUTPUT_PATH%\data\field2d\bg\b07_19.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b07_20.abin" h07_20.bf "%OUTPUT_PATH%\data\field2d\bg\h07_20.bf" "%OUTPUT_PATH%\data\field2d\bg\b07_20.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b08_01.abin" h08_01.bf "%OUTPUT_PATH%\data\field2d\bg\h08_01.bf" "%OUTPUT_PATH%\data\field2d\bg\b08_01.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b08_02.abin" h08_02.bf "%OUTPUT_PATH%\data\field2d\bg\h08_02.bf" "%OUTPUT_PATH%\data\field2d\bg\b08_02.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b08_09.abin" h08_09.bf "%OUTPUT_PATH%\data\field2d\bg\h08_09.bf" "%OUTPUT_PATH%\data\field2d\bg\b08_09.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b08_10.abin" h08_10.bf "%OUTPUT_PATH%\data\field2d\bg\h08_10.bf" "%OUTPUT_PATH%\data\field2d\bg\b08_10.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b09_01.abin" h09_01.bf "%OUTPUT_PATH%\data\field2d\bg\h09_01.bf" "%OUTPUT_PATH%\data\field2d\bg\b09_01.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b09_02.abin" h09_02.bf "%OUTPUT_PATH%\data\field2d\bg\h09_02.bf" "%OUTPUT_PATH%\data\field2d\bg\b09_02.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b09_08.abin" h09_08.bf "%OUTPUT_PATH%\data\field2d\bg\h09_08.bf" "%OUTPUT_PATH%\data\field2d\bg\b09_08.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b09_09.abin" h09_09.bf "%OUTPUT_PATH%\data\field2d\bg\h09_09.bf" "%OUTPUT_PATH%\data\field2d\bg\b09_09.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b10_01.abin" h10_01.bf "%OUTPUT_PATH%\data\field2d\bg\h10_01.bf" "%OUTPUT_PATH%\data\field2d\bg\b10_01.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b10_02.abin" h10_02.bf "%OUTPUT_PATH%\data\field2d\bg\h10_02.bf" "%OUTPUT_PATH%\data\field2d\bg\b10_02.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b12_01.abin" h12_01.bf "%OUTPUT_PATH%\data\field2d\bg\h12_01.bf" "%OUTPUT_PATH%\data\field2d\bg\b12_01.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_02.abin" h14_02.bf "%OUTPUT_PATH%\data\field2d\bg\h14_02.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_02.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_03.abin" h14_03.bf "%OUTPUT_PATH%\data\field2d\bg\h14_03.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_03.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_06.abin" h14_06.bf "%OUTPUT_PATH%\data\field2d\bg\h14_06.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_06.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_07.abin" h14_07.bf "%OUTPUT_PATH%\data\field2d\bg\h14_07.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_07.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_08.abin" h14_08.bf "%OUTPUT_PATH%\data\field2d\bg\h14_08.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_08.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_09.abin" h14_09.bf "%OUTPUT_PATH%\data\field2d\bg\h14_09.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_09.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_10.abin" h14_10.bf "%OUTPUT_PATH%\data\field2d\bg\h14_10.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_10.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_11.abin" h14_11.bf "%OUTPUT_PATH%\data\field2d\bg\h14_11.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_11.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_12.abin" h14_12.bf "%OUTPUT_PATH%\data\field2d\bg\h14_12.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_12.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_13.abin" h14_13.bf "%OUTPUT_PATH%\data\field2d\bg\h14_13.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_13.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_14.abin" h14_14.bf "%OUTPUT_PATH%\data\field2d\bg\h14_14.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_14.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_15.abin" h14_15.bf "%OUTPUT_PATH%\data\field2d\bg\h14_15.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_15.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b14_16.abin" h14_16.bf "%OUTPUT_PATH%\data\field2d\bg\h14_16.bf" "%OUTPUT_PATH%\data\field2d\bg\b14_16.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b15_18.abin" h15_18.bf "%OUTPUT_PATH%\data\field2d\bg\h15_18.bf" "%OUTPUT_PATH%\data\field2d\bg\b15_18.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b35_03.abin" h35_03.bf "%OUTPUT_PATH%\data\field2d\bg\h35_03.bf" "%OUTPUT_PATH%\data\field2d\bg\b35_03.abin"
-%PAKPACK% replace "%OUTPUT_PATH%\data\field2d\bg\b37_02.abin" h37_02.bf "%OUTPUT_PATH%\data\field2d\bg\h37_02.bf" "%OUTPUT_PATH%\data\field2d\bg\b37_02.abin"
-	goto:deletebfs
+%COMPILER% .\hook\h06_01.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_01\h06_01.bf" -Hook
+%COMPILER% .\hook\h06_02.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_02\h06_02.bf" -Hook
+%COMPILER% .\hook\h06_03.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_03\h06_03.bf" -Hook
+%COMPILER% .\hook\h06_04.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_04\h06_04.bf" -Hook
+%COMPILER% .\hook\h06_05.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_05\h06_05.bf" -Hook
+%COMPILER% .\hook\h06_07.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_07\h06_07.bf" -Hook
+%COMPILER% .\hook\h06_11.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_11\h06_11.bf" -Hook
+%COMPILER% .\hook\h06_16.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_16\h06_16.bf" -Hook
+%COMPILER% .\hook\h06_20.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b06_20\h06_20.bf" -Hook
+%COMPILER% .\hook\h07_01.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_01\h07_01.bf" -Hook
+%COMPILER% .\hook\h07_02.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_02\h07_02.bf" -Hook
+%COMPILER% .\hook\h07_02.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\p07_02\bg\b07_02\h07_02.bf" -Hook
+%COMPILER% .\hook\h07_03.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_03\h07_03.bf" -Hook
+%COMPILER% .\hook\h07_04.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_04\h07_04.bf" -Hook
+%COMPILER% .\hook\h07_05.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_05\h07_05.bf" -Hook
+%COMPILER% .\hook\h07_06.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_06\h07_06.bf" -Hook
+%COMPILER% .\hook\h07_09.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_09\h07_09.bf" -Hook
+%COMPILER% .\hook\h07_19.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_19\h07_19.bf" -Hook
+%COMPILER% .\hook\h07_20.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b07_20\h07_20.bf" -Hook
+%COMPILER% .\hook\h08_01.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b08_01\h08_01.bf" -Hook
+%COMPILER% .\hook\h08_02.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b08_02\h08_02.bf" -Hook
+%COMPILER% .\hook\h08_09.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b08_09\h08_09.bf" -Hook
+%COMPILER% .\hook\h08_10.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b08_10\h08_10.bf" -Hook
+%COMPILER% .\hook\h09_01.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b09_01\h09_01.bf" -Hook
+%COMPILER% .\hook\h09_02.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b09_02\h09_02.bf" -Hook
+%COMPILER% .\hook\h09_08.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b09_08\h09_08.bf" -Hook
+%COMPILER% .\hook\h09_09.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b09_09\h09_09.bf" -Hook
+%COMPILER% .\hook\h10_01.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b10_01\h10_01.bf" -Hook
+%COMPILER% .\hook\h10_02.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b10_02\h10_02.bf" -Hook
+%COMPILER% .\hook\h12_01.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b12_01\h12_01.bf" -Hook
+%COMPILER% .\hook\h14_02.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_02\h14_02.bf" -Hook
+%COMPILER% .\hook\h14_03.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_03\h14_03.bf" -Hook
+%COMPILER% .\hook\h14_06.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_06\h14_06.bf" -Hook
+%COMPILER% .\hook\h14_07.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_07\h14_07.bf" -Hook
+%COMPILER% .\hook\h14_08.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_08\h14_08.bf" -Hook
+%COMPILER% .\hook\h14_09.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_09\h14_09.bf" -Hook
+%COMPILER% .\hook\h14_10.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_10\h14_10.bf" -Hook
+%COMPILER% .\hook\h14_11.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_11\h14_11.bf" -Hook
+%COMPILER% .\hook\h14_12.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_12\h14_12.bf" -Hook
+%COMPILER% .\hook\h14_13.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_13\h14_13.bf" -Hook
+%COMPILER% .\hook\h14_14.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_14\h14_14.bf" -Hook
+%COMPILER% .\hook\h14_15.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_15\h14_15.bf" -Hook
+%COMPILER% .\hook\h14_16.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b14_16\h14_16.bf" -Hook
+%COMPILER% .\hook\h15_18.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b15_18\h15_18.bf" -Hook
+%COMPILER% .\hook\h35_03.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b35_03\h35_03.bf" -Hook
+%COMPILER% .\hook\h37_02.flow -Compile -OutFormat V1 -Library P3P -Encoding P3 -Out "%OUTPUT_PATH%\data\field2d\bg\b37_02\h37_02.bf" -Hook
